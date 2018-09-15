17bb7d174bdce7ed7b472e2ad9b5c19c54242bd6
EasonMar <12054648@qq.com>
2018-09-15 12:03:43 +0800

    cross-origin

--- Practice/PHP_basic/ajax.php
+++ Practice/PHP_basic/ajax.php
@@ -1,5 +1,6 @@
 <?php
 	header("Content-type:application/json");
+	header('Access-Control-Allow-Origin:*'); // 允许跨域访问
 	$username = $_REQUEST['username'];
 	$password = $_REQUEST['password'];
 	// 使用关联数组装载对象，传到前端就是对象了
